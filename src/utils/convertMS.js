2fcc5388d8741f733d9ec18bc9689f978533f4be
jigsaw <m@jgs.me>
2016-09-14 13:59:45 +0900

    Complement time with zero

--- src/utils/convertMS.js
+++ src/utils/convertMS.js
@@ -1,4 +1,5 @@
 export default function convertMS (n) {
   const d = new Date(n)
-  return `${d.getHours() - 9}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}`
+  const comp = (n) => n.toString().length === 1 ? `0${n}` : n
+  return `${comp(d.getHours() - 9)}:${comp(d.getMinutes())}:${comp(d.getSeconds())}:${comp(d.getMilliseconds())}`
 }
